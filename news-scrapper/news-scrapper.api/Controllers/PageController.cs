8b75a470e7072207821af3292f0ffdaad0bc9b43
Michał Kruszewski <kruszewski2@gmail.com>
2021-04-25 18:11:23 +0200

    feat: initialize api project

---
+++ news-scrapper/news-scrapper.api/Controllers/PageController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace news_scrapper.api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PageController : ControllerBase
+    {
+        private readonly ILogger<PageController> _logger;
+
+        public PageController(ILogger<PageController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public async Task<string> GetScrappedPage()
+        {
+            return "hello";
+        }
+    }
+}
