fedc6a882610d74fa6a53add8d88d2b39c9b6e15
Michał Kruszewski <kruszewski2@gmail.com>
2021-04-27 08:47:37 +0200

    feat: initialize domain project

---
+++ news-scrapper/news-scrapper.domain/Article.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace news_scrapper.domain
+{
+    public class Article
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
