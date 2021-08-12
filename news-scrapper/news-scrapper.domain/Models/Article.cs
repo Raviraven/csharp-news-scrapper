409eb9958ac66f9fa84238b997bfe1a4a2060130
Michał Kruszewski <kruszewski2@gmail.com>
2021-08-12 16:04:38 +0200

    refactor: add parameters to article model

--- news-scrapper/news-scrapper.domain/Models/Article.cs
+++ news-scrapper/news-scrapper.domain/Models/Article.cs
@@ -4,9 +4,14 @@ namespace news_scrapper.domain.Models
 {
     public class Article
     {
+        public int Id { get; set; }
+        public DateTime DateScrapped { get; set; }
         public string Title { get; set; }
         public string Url { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set; }
+
+        //?
+        public int WebsiteDetailsId { get; set; }
     }
 }
