c472421c44565cb7b29d5ce844da394c5e42fa5f
Michał Kruszewski <kruszewski2@gmail.com>
2021-08-12 16:03:28 +0200

    feat: add base repository

---
+++ news-scrapper/news-scrapper.application/Repositories/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace news_scrapper.application.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        void Delete(TEntity entityToDelete);
+        void Delete(object id);
+        IEnumerable<TEntity> Get(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = "");
+        
+        
+        TEntity GetById(object id);
+        IEnumerable<TEntity> GetWithRawSql(string query, params object[] parameters);
+        TEntity Insert(TEntity entity);
+        TEntity Update(TEntity entityToUpdate);
+
+    }
+}
---
+++ news-scrapper/news-scrapper.infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore;
+using news_scrapper.application.Repositories;
+using news_scrapper.domain.DBModels;
+using news_scrapper.infrastructure.DbAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace news_scrapper.infrastructure.Repositories
+{
+    public class BaseRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        internal PostgreSqlContext context;
+        internal DbSet<TEntity> dbSet;
+
+        public BaseRepository(PostgreSqlContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<TEntity>();
+        }
+
+        public void Delete(TEntity entityToDelete)
+        {
+            if(context.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                dbSet.Attach(entityToDelete);
+            }
+            dbSet.Remove(entityToDelete);
+        }
+
+        public void Delete(object id)
+        {
+            TEntity entityToDelete = dbSet.Find(id);
+            Delete(entityToDelete);
+        }
+
+        public IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null, 
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, string includeProperties = "")
+        {
+            IQueryable<TEntity> query = dbSet;
+
+            if(filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+
+            if(orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+
+            return query.ToList();
+        }
+
+        public TEntity GetById(object id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public IEnumerable<TEntity> GetWithRawSql(string query, params object[] parameters)
+        {
+            return dbSet.FromSqlRaw(query, parameters).ToList();
+        }
+
+        public TEntity Insert(TEntity entity)
+        {
+            dbSet.Add(entity);
+            return entity;
+        }
+
+        public TEntity Update(TEntity entityToUpdate)
+        {
+            //dbSet.Attach(entityToUpdate);
+            //context.Entry(entityToUpdate).State = EntityState.Modified;
+            dbSet.Update(entityToUpdate);
+            return entityToUpdate;
+        }
+    }
+}
