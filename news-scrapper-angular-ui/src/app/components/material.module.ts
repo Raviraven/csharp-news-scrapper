d60cd524cccc36d9b64a67916039f9a9b00d236e
Michał Kruszewski <kruszewski2@gmail.com>
2022-05-03 22:22:11 +0200

    feat: add loading spinner

--- news-scrapper-angular-ui/src/app/components/material.module.ts
+++ news-scrapper-angular-ui/src/app/components/material.module.ts
@@ -3,8 +3,14 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
-  exports: [MatButtonModule, MatButtonToggleModule, MatIconModule],
+  exports: [
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+  ],
 })
 export class MaterialModule {}
